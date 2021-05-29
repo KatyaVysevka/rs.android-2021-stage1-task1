04bc19b1eab026f4b346426e96574c9d154c2cd9
KatyaVysevka <katrina_v89@mail.ru>
2021-05-29 17:16:27 +0300

    tasks completed

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,32 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        // throw NotImplementedError("Not implemented")
+        var answer = sadArray
+        while (!isHappy(answer)) {
+            answer = oneStep(answer)
+        }
+        return answer
+    }
+
+    private fun isHappy(answer: IntArray): Boolean {
+        for (i in 1 .. answer.size - 2) {
+            if (answer[i] > answer[i-1]+answer[i+1])
+                return false
+        }
+        return true
+    }
+
+    private fun oneStep(sadArray: IntArray): IntArray {
+        // throw NotImplementedError("Not implemented")
+        var filtered = sadArray
+            .filterIndexed { index, it
+                ->
+                index == 0
+                        || index == sadArray.size - 1
+                        || it < sadArray[index - 1] + sadArray[index + 1]
+            }
+            .toIntArray()
+        return filtered
     }
 }
--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -4,6 +4,54 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        //throw NotImplementedError("Not implemented")
+        val array = inputString.toCharArray()
+        val stringSize = inputString.count() - 1
+        val answerArray = ArrayList<String>()
+        var isRoundBracket = false
+        var roundBracket = ""
+        var isSquareBracket = false
+        var squareBracket = ""
+        var isAngleBracket = false
+        var angleBracket = ""
+        for (i in 0..stringSize) {
+
+            if (array[i] == ')' && roundBracket.length > 0) {
+                isRoundBracket = false
+                answerArray.add(roundBracket)
+                roundBracket = ""
+            }
+            if (array[i] == ']' && squareBracket.length > 0) {
+                isSquareBracket = false
+                answerArray.add(squareBracket)
+                squareBracket = ""
+            }
+            if (array[i] == '>' && angleBracket.length > 0) {
+                isAngleBracket = false
+                answerArray.add(angleBracket)
+                angleBracket = ""
+            }
+            if (isRoundBracket) roundBracket += array[i]
+            if (isSquareBracket) squareBracket += array[i]
+            if (isAngleBracket) angleBracket += array[i]
+            if (array[i] == '(') {
+                isRoundBracket = true
+                roundBracket = ""
+            }
+            if (array[i] == '[') {
+                isSquareBracket = true
+                squareBracket = ""
+            }
+            if (array[i] == '<') {
+                isAngleBracket = true
+                angleBracket = ""
+            }
+
+
+        }
+        return answerArray.toTypedArray()
     }
 }
+
+
+
